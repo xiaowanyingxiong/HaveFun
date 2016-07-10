fddb6c5f92270c809c5885f8b68f6f373d73e53b
jian xiong <xiaowanyingxiong@126.com>
2016-07-10 13:51:07 +0800

    '修改框架'

--- app/src/main/java/xiongjian/com/havefun/Myapplication.java
+++ app/src/main/java/xiongjian/com/havefun/Myapplication.java
@@ -2,8 +2,6 @@ package xiongjian.com.havefun;
 
 import android.app.Application;
 
-import com.android.core.control.logcat.Logcat;
-import com.android.core.model.control.LogicProxy;
 
 /**
  * Created by Administrator on 2016/7/3.
@@ -12,7 +10,5 @@ public class Myapplication extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
-        LogicProxy.getInstance().init();
-        Logcat.init("com.android.logcat").hideThreadInfo().methodCount(3);
     }
 }
